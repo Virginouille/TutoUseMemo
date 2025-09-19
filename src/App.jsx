7736f778c64e698d32a883195d6e46c0ae6f45ad
Virginie <143293036+Virginouille@users.noreply.github.com>
2025-09-19 16:52:13 +0200

    début structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
 import { useState } from 'react'
+import { Input } from './components/Input'
 
 function App() {
 
-  return (
-    <>
+  const [firstname, setFirstname] = useState('John')
+  const [password, setPassword] = useState('Mot de passe')
+  const security = passwordSecurity(password)
 
-    </>
-  )
+  return <div className='container my-3 vstack gap-2'>
+    <Input
+      label="Nom d'utilisateur"
+      value={firstname}
+      onChange={setFirstname}
+    />
+
+    <div>
+      <Input
+        label="Password"
+        type="password"
+        value={password}
+        onChange={setPassword}
+      />
+    </div>
+
+    Sécurité : {security}
+  </div>
 }
 
 export default App

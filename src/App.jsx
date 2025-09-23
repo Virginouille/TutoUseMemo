a52c2a229edfcbedd28a06f88554f774636db8d6
Virginie <143293036+Virginouille@users.noreply.github.com>
2025-09-23 10:23:35 +0200

    hooker reducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,63 @@
-import { useEffect, useId, useMemo, useRef, useState } from 'react'
-import { Input } from './components/Input'
-import { useIncrement } from './hooks/useIncrement'
-import { useDocumentTitle } from './hooks/useDocumentTitle'
+import { useReducer } from 'react'
+
+
+function reducer(state, action) {
+  if (action.type === 'REMOVE_TODO') {
+    return {
+      ...state,
+      todos: state.todos.filter(todo => todo !== action.payload)
+    }
+  }
+  if (action.type === 'TOGGLE_TODO') {
+    return {
+      ...state,
+      todos: state.todos.map(todo => todo === action.payload ? {
+        ...todo,
+        checked: !todo.checked
+      } : todo)
+    }
+  }
+  if (action.type === 'CLEAR_COMPLETED') {
+    return {
+      ...state,
+      todos: state.todos.filter(todo => todo === action.payload ? {
+        ...todo,
+        checked: !todo.checked
+      } : todo)
+    }
+  }
+  return state
+}
+
 
 function App() {
 
-  const [count, increment, decrement] = useIncrement(0)
-
-  const [name, setName] = useState('')
-  useDocumentTitle(name ? `Editer + ${name}` : null)
+  const [state, dispatch] = useReducer(reducer, {
+    todos: [{
+      name: 'faire les courses',
+      checked: false
+    }, {
+      name: 'ranger les courses',
+      checked: false
+    }, {
+      name: 'faire à manger',
+      checked: false
+    }]
+  })
 
   return <div>
-    <Input value={name} onChange={setName} label="Nom" />
-    Compteur: {count}
-    <button onClick={increment}>Incrémenter</button>
-    <button onClick={decrement}>Décrémenter</button>
+    <ul>
+      {state.todos.map(todo => (<li
+        key={todo.name}
+      >
+        <input type="checkbox" onChange={() => dispatch({ type: 'TOGGLE_TODO', payload: todo })} checked={todo.checked}></input>
+        {todo.name}
+        <button onClick={() => dispatch({ type: 'REMOVE_TODO', payload: todo })}>Supprimer</button>
+      </li>))}
+    </ul>
+    <button onClick={() => dispatch({ type: 'CLEAR_COMPLETED' })}>Supprimer les tâches accomplies</button>
   </div>
+
 }
 
 export default App
